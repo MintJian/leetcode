a1b9d511794b9c30fe79e1020f84fca9ea596435
MintJian <mintjian@outlook.com>
2020-05-09 01:18:26 +0800

    add file

--- 69.swift
+++ 69.swift
@@ -1,3 +1,28 @@
+//2020.5
+class Solution {
+    func mySqrt(_ x: Int) -> Int {
+        var left = 0, right = x
+        
+        while left <= right {
+            let mid = (left + right) / 2
+            if mid * mid <= x && (mid + 1) * (mid + 1) > x {
+                return mid
+            }
+            if (mid + 1) * (mid + 1) == x {
+                return mid + 1
+            }
+            if (mid + 1) * (mid + 1) < x {
+                left = mid + 1
+            }
+            if mid * mid > x {
+                right = mid - 1
+            }
+        }
+        
+        return -1
+    }
+}
+
 class Solution {
     func mySqrt(_ x: Int) -> Int {
         for i in 0...x {
